b4562babc5e5147a7196b79cd1226f8232a36de1
aarav <pragya.singhvi@gmail.com>
2022-03-17 20:09:35 -0400

    CRUD operations for customers in api.

--- CustomerMicroService/Controllers/CustomerController.cs
+++ CustomerMicroService/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CustomerMicroService.Controllers
@@ -8,11 +9,42 @@ namespace CustomerMicroService.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        [HttpGet("Getme")]
+        [HttpGet("{id}")]
         public async Task<CustomerDataModel> GetCustomerDataModelAsync(int id)
         {
             DatabaseService databaseService = new DatabaseService();
             return await databaseService.GetCustomerById(id);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CustomerDataModel>> GetAllCustomersAsync()
+        {
+            DatabaseService databaseService = new DatabaseService();
+            return await databaseService.GetAllCustomers();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDataModel> AddCustomerAsync(CustomerDataModel customer)
+        {
+            DatabaseService databaseService = new DatabaseService();
+
+            return await databaseService.AddCustomer(customer);
+        }
+
+        [HttpPost]
+        public async Task<CustomerDataModel> UpdateCustomerAsync(CustomerDataModel customer)
+        {
+            DatabaseService databaseService = new DatabaseService();
+
+            return await databaseService.UpdateCustomer(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomerAsync(int id)
+        {
+            DatabaseService databaseService = new DatabaseService();
+
+            return await databaseService.DeleteCustomer(id);
         }
     }
 }
--- CustomerMicroService/DatabaseService.cs
+++ CustomerMicroService/DatabaseService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,6 +33,82 @@ namespace CustomerMicroService
             }
             return customer;
 
-        } 
+        }
+
+        public async Task<IEnumerable<CustomerDataModel>> GetAllCustomers()
+        {
+            var customers = new List<CustomerDataModel>();
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                NpgsqlCommand npgsqlCommand = conn.CreateCommand();
+                npgsqlCommand.CommandText = "Select * from \"Customer\"";
+                using (NpgsqlDataReader npgsqlReader = await npgsqlCommand.ExecuteReaderAsync())
+                {
+                    while (npgsqlReader.Read())
+                    {
+                        var customer = new CustomerDataModel();
+                        customer.Id = (int)npgsqlReader["Id"];
+                        customer.Name = (string)npgsqlReader["Name"];
+                        customers.Add(customer);
+                    }
+                }
+                conn.Close();
+                conn.Dispose();
+            }
+            return customers;
+
+        }
+
+        public async Task<CustomerDataModel> AddCustomer(CustomerDataModel customer)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                NpgsqlCommand npgsqlCommand = conn.CreateCommand();
+                npgsqlCommand.CommandText = "INSERT INTO \"Customer\" (id, name) VALUES (@id, @name);";
+                npgsqlCommand.Parameters.Add("id", NpgsqlTypes.NpgsqlDbType.Integer).Value = customer.Id;
+                npgsqlCommand.Parameters.Add("name", NpgsqlTypes.NpgsqlDbType.Varchar).Value = customer.Name;
+                await npgsqlCommand.ExecuteNonQueryAsync();
+                conn.Close();
+                conn.Dispose();
+            }
+            return customer;
+
+        }
+
+        public async Task<CustomerDataModel> UpdateCustomer(CustomerDataModel customer)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                NpgsqlCommand npgsqlCommand = conn.CreateCommand();
+                npgsqlCommand.CommandText = "UPDATE \"Customer\" SET id =@id, name =@name;";
+                npgsqlCommand.Parameters.Add("id", NpgsqlTypes.NpgsqlDbType.Integer).Value = customer.Id;
+                npgsqlCommand.Parameters.Add("name", NpgsqlTypes.NpgsqlDbType.Varchar).Value = customer.Name;
+                await npgsqlCommand.ExecuteNonQueryAsync();
+                conn.Close();
+                conn.Dispose();
+            }
+            return customer;
+
+        }
+
+        public async Task<bool> DeleteCustomer(int id)
+        {
+            var rowsAffected = 0;
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+                NpgsqlCommand npgsqlCommand = conn.CreateCommand();
+                npgsqlCommand.CommandText = "DELETE FROM \"Customer\" WHERE id =@id;";
+                npgsqlCommand.Parameters.Add("id", NpgsqlTypes.NpgsqlDbType.Integer).Value = id;
+                rowsAffected = await npgsqlCommand.ExecuteNonQueryAsync();         
+                conn.Close();
+                conn.Dispose();
+            }
+            return rowsAffected > 0;
+
+        }
     }
 }
